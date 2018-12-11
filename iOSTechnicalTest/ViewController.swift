7075400770f58c547ef75157483cf0c16cae613c
Casey <casey@lomotif.com>
2018-12-11 15:11:55 +0800

    Initial Commit

---
+++ iOSTechnicalTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSTechnicalTest
+//
+//  Created by Casey Law on 11/12/18.
+//  Copyright © 2018 Lomotif. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
