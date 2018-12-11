756ba8dbe35df9d42e36b6411345ffa9212527d5
Casey <casey@lomotif.com>
2018-12-11 15:19:19 +0800

    initial commit

---
+++ iOSTechnicalTest/VideoPreviewViewController.swift
@@ -0,0 +1,30 @@
+//
+//  VideoPreviewViewController.swift
+//  iOS Technical Test
+//
+//  Created by Casey Law on 11/12/18.
+//  Copyright © 2018 Lomotif. All rights reserved.
+//
+
+import UIKit
+
+class VideoPreviewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
